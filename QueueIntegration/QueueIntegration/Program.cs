b87b179ce623295b6fbd96806cdc2fdf6cd37c7a
Konrad Parchański <konradparchanski@gitlab.com>
2020-07-24 15:30:43 +0200

    Test

---
+++ QueueIntegration/QueueIntegration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QueueIntegration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
